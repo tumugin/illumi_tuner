aa7c3fe18ff01d57515178904b27c2bbd0dcb28e
Kazuki Oishi <oishikazuki@gmail.com>
2019-04-23 16:09:40 +0900

    標準色から選択させるアルゴリズムを実装した

--- src/service/penlight/abstract-penlight.ts
+++ src/service/penlight/abstract-penlight.ts
@@ -30,7 +30,8 @@ export default abstract class AbstractPenlight {
         return mapItem
       })
     }
-    return arrayOfIdol.map(item => {
+    // search the nearest color
+    const searchResult = arrayOfIdol.map(item => {
       const idolColorRGB = ColorUtils.convertColorCodeToRGB(item.colorHEX)
       const colorDiffList = this.availableColors.map(penlightColorItem => {
         const mapValue: IColorDiffWithIPenlightColor = {
@@ -45,5 +46,43 @@ export default abstract class AbstractPenlight {
       }
       return searchResultMapValue
     })
+
+    // if not using basic color priority mode
+    if (!useBasicColor) {
+      return searchResult
+    }
+
+    // using basic color option
+    // すでに選択されている標準色のみを抜き出して、他のものもそれに合わせるようにする
+    // TODO: 許容量もパラメータに設定できるようにする
+
+    // 標準色のみを求める
+    const selectedBasicColors = searchResult
+      .filter(item => item.penlightColor.isBasicColor)
+      .map(item => item.penlightColor)
+    // 標準色のみで表現可能なアイドルを取得する
+    const basicColorIdols = searchResult
+      .filter(item => !item.penlightColor.isBasicColor)
+    // そうでないアイドルの色を標準色で表現させる
+    const notBasicColorIdols = searchResult
+      .filter(item => !item.penlightColor.isBasicColor)
+      .map(item => item.nameAndColor)
+      .map(item => {
+        const idolColorRGB = ColorUtils.convertColorCodeToRGB(item.colorHEX)
+        const colorDiffList = selectedBasicColors.map(penlightColorItem => {
+          const mapValue: IColorDiffWithIPenlightColor = {
+            penlightColor: penlightColorItem,
+            diff: Ciede2000.calculateCiede2000FromRGB(penlightColorItem.colorRGB, idolColorRGB)
+          }
+          return mapValue
+        })
+        const searchResultMapValue: INameAndColorWithPenlightColor = {
+          nameAndColor: item,
+          penlightColor: colorDiffList.sort((a, b) => a.diff - b.diff)[0].penlightColor
+        }
+        return searchResultMapValue
+      })
+    // それらを結合する
+    return [...basicColorIdols, ...notBasicColorIdols]
   }
 }
