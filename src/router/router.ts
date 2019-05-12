3e818cfe6e540b446a5efcef7d626f3fb2a6d652
Kazuki Oishi <oishikazuki@gmail.com>
2019-05-13 03:48:54 +0900

    OGP対応とタイトル周りを修正した

--- src/router/router.ts
+++ src/router/router.ts
@@ -2,23 +2,43 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 Vue.use(Router)
-export default new Router({
+
+const router = new Router({
   routes: [
     {
       path: '/',
       name: 'Main',
-      component: async () => await require('../components/main-view.vue')
+      component: async () => await import('../components/main-view.vue'),
+      meta: {
+        title: 'IllumiTuner β'
+      }
     },
     {
       path: '/about',
       name: 'About',
-      component: async () => await require('../components/about-page.vue')
+      component: async () => await import('../components/about-page.vue'),
+      meta: {
+        title: 'これなあに - IllumiTuner'
+      }
     },
     {
       path: '/terms',
       name: 'Terms',
-      component: async () => await require('../components/terms-page.vue')
+      component: async () => await import('../components/terms-page.vue'),
+      meta: {
+        title: '利用規約 - IllumiTuner'
+      }
     }
   ],
   mode: 'history'
 })
+
+router.afterEach(to => {
+  if (to.meta.title) {
+    document.title = to.meta.title
+  } else {
+    document.title = 'IllumiTuner'
+  }
+})
+
+export default router
