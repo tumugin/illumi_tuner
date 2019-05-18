4bcc6aead0b4e3730e70e2a6cc124aad70e5b279
Kazuki Oishi <oishikazuki@gmail.com>
2019-05-18 15:01:43 +0900

    モバイル用表示を実装した

--- src/components/live-select-view.vue
+++ src/components/live-select-view.vue
@@ -1,19 +1,36 @@
 <template>
-  <b-dropdown text="公演を選択する">
-    <template v-for="(live, index) in liveList">
-      <b-dropdown-item :key="index" @click="selectLive(live)">
-        <b-badge variant="primary">
-          {{ formatDate(live.liveDate, 'YYYY/MM/DD') }}
-        </b-badge>
-        <b-badge variant="dark">
-          {{ live.liveLocation }}
-        </b-badge>
-        <div>
-          {{ live.liveName }}
+  <div>
+    <b-dropdown text="公演を選択する" @toggle="openMobileModal">
+      <template v-for="(live, index) in liveList">
+        <b-dropdown-item :key="index" @click="selectLive(live)">
+          <b-badge variant="primary">
+            {{ formatDate(live.liveDate, 'YYYY/MM/DD') }}
+          </b-badge>
+          <b-badge variant="dark">
+            {{ live.liveLocation }}
+          </b-badge>
+          <div>
+            {{ live.liveName }}
+          </div>
+        </b-dropdown-item>
+      </template>
+    </b-dropdown>
+    <b-modal :visible="showMobileModal" title="公演を選択してください" hide-footer @close="closeModal">
+      <template v-for="(live, index) in liveList">
+        <div :key="index" @click="selectLive(live)">
+          <b-badge variant="primary">
+            {{ formatDate(live.liveDate, 'YYYY/MM/DD') }}
+          </b-badge>
+          <b-badge variant="dark">
+            {{ live.liveLocation }}
+          </b-badge>
+          <div>
+            {{ live.liveName }}
+          </div>
         </div>
-      </b-dropdown-item>
-    </template>
-  </b-dropdown>
+      </template>
+    </b-modal>
+  </div>
 </template>
 
 <script lang="ts">
@@ -23,6 +40,11 @@ import IllumiTunerVuexModule from '../store/illumi-tuner-vuex-module'
 import ILive from '../models/i-live'
 
 const LiveSelectView = Vue.extend({
+  data() {
+    return {
+      showMobileModal: false
+    }
+  },
   props: {
     liveList: {
       type: Array,
@@ -32,11 +54,20 @@ const LiveSelectView = Vue.extend({
   methods: {
     formatDate: formatDate,
     selectLive(live: ILive) {
+      this.showMobileModal = false
       const selectedIdol = IllumiTunerVuexModule.imasCharacters.filter(item => live.liveActor.includes(item.actor))
       selectedIdol.forEach(item => {
         item.checked = true
         IllumiTunerVuexModule.updateImasCharacter(item, item.key)
       })
+    },
+    openMobileModal() {
+      if (window.innerWidth < 700) {
+        this.showMobileModal = true
+      }
+    },
+    closeModal() {
+      this.showMobileModal = false
     }
   }
 })
