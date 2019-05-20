f48a5e516d216f5bd78677dff3a283a295b94179
Kazuki Oishi <oishikazuki@gmail.com>
2019-05-20 23:52:42 +0900

    addon-storyshots-puppeteerを入れた

--- src/test/mock-idols.ts
+++ src/test/mock-idols.ts
@@ -10,6 +10,24 @@ const MockIdols: INameAndColor[] = [
     actor: '春日未来',
     key: uuidv4(),
     checked: false
+  },
+  {
+    colorHEX: '#F5AD3B',
+    name: '神埼澪',
+    nameKana: 'かんざきみお',
+    title: 'STAR ELEMENTS',
+    actor: '矢吹可奈',
+    key: uuidv4(),
+    checked: false
+  },
+  {
+    colorHEX: '#92CFBB',
+    name: '草薙星蘭',
+    nameKana: 'くさなぎせいらん',
+    title: 'STAR ELEMENTS',
+    actor: '田中琴葉',
+    key: uuidv4(),
+    checked: false
   }
 ]
 
--- src/test/storyshot.test.ts
+++ src/test/storyshot.test.ts
@@ -1,5 +1,9 @@
 import initStoryshots from '@storybook/addon-storyshots'
+// @ts-ignore
+import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer'
+import * as path from 'path'
 
 initStoryshots({
-  configPath: '.storybook/config-jest.ts'
+  configPath: '.storybook/config-jest.ts',
+  test: imageSnapshot({ storybookUrl: 'file://' + path.resolve('./storyshot-public/') })
 })
