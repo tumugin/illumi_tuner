efd6f51fa5ce0e4b8843c8b087f610fca923dd0b
Kazuki Oishi <oishikazuki@gmail.com>
2020-02-22 04:51:41 +0900

    storybookがぶっ壊れてたので直した

---
+++ .storybook/webpack.config.ts
@@ -0,0 +1,22 @@
+import * as path from 'path'
+import * as webpack from 'webpack'
+import custom from '../build/webpack.conf'
+
+module.exports = async ({ config }: { config: webpack.Configuration }) => {
+  config.resolve!.modules = [
+    ...(config.resolve!.modules || []),
+    path.resolve(__dirname, '../'),
+    path.resolve(__dirname, '../src'),
+    path.resolve(__dirname, '../node_modules')
+  ]
+  config.resolve!.extensions!.push('.ts')
+  config.plugins!.push(
+    new webpack.DefinePlugin({
+      IS_STORYSHOT: JSON.stringify(process.env.IS_STORYSHOT)
+    })
+  )
+  return {
+    ...config,
+    module: { ...config.module, rules: custom({}, { mode: 'development' }).module!.rules }
+  }
+}
