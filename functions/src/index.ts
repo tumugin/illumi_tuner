74267f0b49a5f7b8ddcac80ab47fd243f2d6e5a2
Kazuki Oishi <oishikazuki@gmail.com>
2020-03-01 07:30:49 +0900

    firebase functionsでSSRしてみる

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,19 @@
 import * as functions from 'firebase-functions'
+import { createBundleRenderer } from 'vue-server-renderer'
+import * as path from 'path'
+import * as fs from 'fs'
 
-export const vueSSR = functions.https.onRequest((request, response) => {
-  response.send('Hello from Firebase!')
+const renderer = createBundleRenderer(
+  JSON.parse(fs.readFileSync(path.join('.', 'dist', 'vue-ssr-server-bundle.json'), 'utf-8')),
+  {
+    runInNewContext: false,
+    template: fs.readFileSync(path.join('.', 'dist', 'index.ssr.template.html'), 'utf-8'),
+    clientManifest: JSON.parse(fs.readFileSync(path.join('.', 'dist', 'vue-ssr-client-manifest.json'), 'utf-8'))
+  }
+)
+
+export const vueSSRServer = functions.https.onRequest(async (request, response) => {
+  const context = { url: request.url }
+  const html = await renderer.renderToString(context)
+  response.send(html)
 })
