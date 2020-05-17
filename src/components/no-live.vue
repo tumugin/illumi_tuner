9dc3abcb6e3feb5176b4447ab6fc40fa0fcfa081
Kazuki Oishi <oishikazuki@gmail.com>
2020-05-18 05:32:02 +0900

    公演が0件になるとバグる問題を直した

---
+++ src/components/no-live.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="parent">
+    <div class="caption">
+      公演が何もありません...🥺
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'NoLive',
+})
+</script>
+
+<style scoped lang="scss">
+.parent {
+  padding: 20px;
+  .caption {
+    text-align: center;
+    font-size: calc(100% + 0.1vw);
+  }
+}
+</style>
